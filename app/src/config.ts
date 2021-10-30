c4664adf8a389657fe5d426e087f94c373293535
Mohamed Raffik <raffikmohamed7@gmail.com>
2021-10-30 01:37:29 -0400

    wrong domain

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,7 +1,7 @@
 let server_domain = '';
 
 if (process.env.NODE_ENV === 'production') {
-    server_domain = 'dripdrop.com';
+    server_domain = 'dripdrop.icu';
 } else {
     server_domain = 'localhost:5000';
 }
