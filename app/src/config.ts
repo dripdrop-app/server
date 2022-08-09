0bccc4b24e672e6dcf03105be9c34e7418fadac5
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-08-09 18:25:23 -0400

    update domain names

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 const protocol = isProd ? 'wss' : 'ws';
-const server_domain = isProd ? 'dripdrop.icu' : 'localhost:5000';
+const server_domain = isProd ? 'www.dripdrop.icu' : 'localhost:5000';
 
 export const buildWebsocketURL = (endpoint: string) => `${protocol}://${server_domain}/api${endpoint}`;
