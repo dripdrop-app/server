c5476a008d207aac17c8f6dcca5e3061999a3327
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-08-21 21:43:11 -0400

    separate dev and prod setups

---
+++ scripts/web.dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+alembic upgrade head
+npm --prefix app install
+
+uvicorn server.app:app --reload --reload-dir server --host 0.0.0.0 --port $SERVER_PORT &
+SERVER_PID=$!
+npm --prefix app start &
+CLIENT_PID=$!
+
+while true;
+do
+    ps -p $SERVER_PID > /dev/null && ps -p $CLIENT_PID > /dev/null
+    if [[ $? -eq 1 ]]; then
+        exit
+    fi
+    sleep 5
+done
--- scripts/web.sh
+++ scripts/web.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 
 alembic upgrade head
-npm --prefix app install
-
-if [[ $ENV == 'production' ]]; then
-    NODE_ENV=$ENV npm --prefix app run build && mv app/build .
-    gunicorn server.app:app -w 2 -k uvicorn.workers.UvicornWorker -b :$SERVER_PORT -c config.py
-    exit
-fi
-
-
-uvicorn server.app:app --reload --reload-dir server --host 0.0.0.0 --port $SERVER_PORT &
-SERVER_PID=$!
-npm --prefix app start &
-CLIENT_PID=$!
-
-while true;
-do
-    ps -p $SERVER_PID > /dev/null && ps -p $CLIENT_PID > /dev/null
-    if [[ $? -eq 1 ]]; then
-        exit
-    fi
-    sleep 5
-done
+gunicorn server.app:app -w 2 -k uvicorn.workers.UvicornWorker -b :$SERVER_PORT -c config.py
