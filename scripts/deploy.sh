b90c83196a76c1930481de404efa548bb5ec9cd1
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-10-02 23:03:21 +0000

    try to fix git pull from branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,8 @@
 
 [ ! -d $HOME/dripdrop ] || git clone https://github.com/MohamedRaffik/dripdrop
 cd $HOME/dripdrop
-git reset --hard HEAD
-git config pull.ff only       
-git pull
+git checkout master
+git pull origin
 mkdir -p build
 \cp -r $HOME/build $HOME/dripdrop
 \cp $HOME/.env .
