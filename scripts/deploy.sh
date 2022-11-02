66ef5e8c682c4a87b5a3c2399ee60180bf27bdb0
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-11-02 12:53:16 -0400

    don't prune images during build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,6 @@ git pull --rebase
 mkdir -p build
 \cp -r $HOME/build $HOME/dripdrop
 \cp $HOME/.env .
-docker system prune -a -f
 docker compose -f docker-compose.yml build --progress plain
 docker compose -f docker-compose.yml down
 docker compose -f docker-compose.yml up -d
