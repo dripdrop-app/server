07141be96fab8debc4a8a02c156c99388f201014
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-11-07 22:17:03 -0500

    don't allow multiple rebuilds at once

--- scripts/server_deploy.sh
+++ scripts/server_deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-[ ! -d $HOME/dripdrop ] || git clone https://github.com/dripdrop-app/server.git
+while [ -f $HOME/build.txt ]
+do
+    echo 'Waiting on build'
+    sleep 10
+done
+touch $HOME/build.txt[ ! -d $HOME/dripdrop ] || git clone https://github.com/dripdrop-app/server.git
 cd $HOME/dripdrop
 git pull --rebase
 mkdir -p build
@@ -9,3 +14,4 @@ mkdir -p build
 docker compose -f docker-compose.yml build --progress plain
 docker compose -f docker-compose.yml down
 docker compose -f docker-compose.yml up -d
+rm $HOME/build.txt
