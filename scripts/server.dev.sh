b47e854fe6373f99fc999f9f221c8e34b5b8635d
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-11-12 20:10:35 -0500

    test run without bash scripts and env variables in circleci

--- scripts/server.dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-alembic upgrade head
-uvicorn server:app --reload --reload-dir server --host 0.0.0.0 --port $SERVER_PORT --log-config ./config/logging.yml
--- scripts/server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-alembic upgrade head
-gunicorn server:app -w 2 -k uvicorn.workers.UvicornWorker -b :$SERVER_PORT -c ./config/gunicorn.py
