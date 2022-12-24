954b71bdc06228896e55c5102dc770964f541de8
Mohamed Raffik <raffikmohamed7@gmail.com>
2022-12-24 18:08:14 -0500

    see what variables are loaded during tests

--- dripdrop/services/rq.py
+++ dripdrop/services/rq.py
@@ -2,4 +2,5 @@ from dripdrop.settings import settings
 from redis import Redis
 from rq import Queue
 
+print(settings)
 queue = Queue(connection=Redis.from_url(settings.redis_url), default_timeout=-1)
