5c91998312e34788f73a7bfa0e8ea86a9b751622
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-01-05 21:49:18 +0000

    add logging argument to worker

--- worker.py
+++ worker.py
@@ -1,19 +1,25 @@
+import argparse
 import logging.config
 import os
 import yaml
 from redis import Redis
 from rq import Connection, Worker
+
 from dripdrop.settings import settings
 
 
-def run_worker():
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--logging", action="store_true")
+    args = parser.parse_args()
+
+    if args.logging:
+        with open(
+            os.path.join(os.path.dirname(__file__), "./config/logging.yml")
+        ) as file:
+            loaded_config = yaml.safe_load(file)
+            logging.config.dictConfig(loaded_config)
+
     with Connection(connection=Redis.from_url(settings.redis_url)):
         worker = Worker(["default"])
         worker.work(with_scheduler=True)
-
-
-if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), "./config/logging.yml")) as file:
-        loaded_config = yaml.safe_load(file)
-        logging.config.dictConfig(loaded_config)
-    run_worker()
