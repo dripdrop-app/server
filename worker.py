0922ff3bc8e55fec2417b41962233cafe99f8bff
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-02-18 13:44:32 -0500

    use logging format for workers, only output rq logs to stdout

--- worker.py
+++ worker.py
@@ -1,14 +1,18 @@
-import argparse
 import logging.config
 import os
 import yaml
 from redis import Redis
 from rq import Connection, Worker
 
-from dripdrop.settings import settings
+from dripdrop.settings import settings, ENV
 
 
 if __name__ == "__main__":
+    if settings.env == ENV.PRODUCTION:
+        with open(os.path.join(os.path.dirname(__file__), "./logging.yml")) as file:
+            loaded_config = yaml.safe_load(file)
+            logging.config.dictConfig(loaded_config)
+
     with Connection(connection=Redis.from_url(settings.redis_url)):
         worker = Worker(["default"])
         worker.work(with_scheduler=True)
