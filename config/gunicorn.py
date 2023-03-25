c413ca928c520dcf676dd031e02b1da3fcfea064
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-03-25 18:41:21 -0400

    remove gunicorn config

--- config/gunicorn.py
+++
@@ -1,7 +0,0 @@
-import logging.config
-import os
-import yaml
-
-with open(os.path.join(os.path.dirname(__file__), "./logging.yml")) as file:
-    loaded_config = yaml.safe_load(file)
-    logging.config.dictConfig(loaded_config)
