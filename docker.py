8948005fb0324e946c588d6fb5547bf0b508a196
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-04-07 21:49:59 -0400

    update docker script

--- docker.py
+++ docker.py
@@ -16,42 +16,24 @@ class DockerInterface:
         compose_file: str = ...,
         env_file: str = ...,
         context: str = ...,
-        tag: str = ...,
+        project: str = ...,
     ) -> None:
-        self._REGISTRY_PORT = 5001
-        self._REGISTRY_ADDRESS = f"localhost:{self._REGISTRY_PORT}"
         self._docker_file = docker_file
         self._compose_file = compose_file
         self._env_file = env_file
         self._context = context
-        self._tag = tag
-        self._image = f"{self._REGISTRY_ADDRESS}/{tag}"
+        self._project = project
         self._env = None
-
-    def _build_docker_image(self):
-        subprocess.run(
-            [
-                "docker",
-                "image",
-                "build",
-                "--tag",
-                self._image,
-                "-f",
-                self._docker_file,
-                self._context,
-            ]
-        ).check_returncode()
-        subprocess.run(["docker", "image", "push", self._image]).check_returncode()
 
     def remove_services(self):
         subprocess.run(
-            ["docker", "compose", "-p", self._tag, "-f", self._compose_file, "down"],
+            ["docker", "compose", "-p", self._project, "-f", self._compose_file, "down"],
             env=self._load_environment_variables(),
         ).check_returncode()
 
     def _load_environment_variables(self):
         if self._env is None:
-            self._env = {"IMAGE": self._image}
+            self._env = {}
             lines = []
             with open(self._env_file) as f:
                 lines = f.readlines()
@@ -66,7 +48,7 @@ class DockerInterface:
                 "docker",
                 "compose",
                 "-p",
-                self._tag,
+                self._project,
                 "-f",
                 self._compose_file,
                 "up",
@@ -75,28 +57,7 @@ class DockerInterface:
             env=self._load_environment_variables(),
         ).check_returncode()
 
-    def _create_registry(self):
-        process = subprocess.run(["docker", "inspect", "registry"])
-        if process.returncode != 0:
-            subprocess.run(
-                [
-                    "docker",
-                    "run",
-                    "-d",
-                    "-p",
-                    f"{self._REGISTRY_PORT}:5000",
-                    "--restart=always",
-                    "--mount",
-                    "type=volume,src=registry-volume,dst=/var/lib/registry",
-                    "--name",
-                    "registry",
-                    "registry:2",
-                ]
-            ).check_returncode()
-
     def deploy(self, env: str = ...):
-        self._create_registry()
-        self._build_docker_image()
         if env == DEVELOPMENT:
             self.remove_services()
         self._deploy_services()
@@ -118,12 +79,12 @@ if __name__ == "__main__":
     context = os.path.join(current_path, ".")
 
     development_options = {
-        "tag": "dripdrop-dev",
+        "project": "dripdrop-dev",
         "docker_file": os.path.join(current_path, "./dockerfiles/Dockerfile.dev"),
         "compose_file": os.path.join(current_path, "docker-compose.dev.yml"),
     }
     production_options = {
-        "tag": "dripdrop",
+        "project": "dripdrop",
         "docker_file": os.path.join(current_path, "./dockerfiles/Dockerfile"),
         "compose_file": os.path.join(current_path, "docker-compose.prod.yml"),
     }
