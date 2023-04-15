996dede8502dfe4a1b5eb89974c015a229c53667
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-04-15 02:35:05 -0400

    increase workers for server and worker processes

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,7 +6,7 @@ if [ "$ENV" == "development" ]
 then
     poetry run uvicorn dripdrop.app:app --reload --reload-dir dripdrop --host 0.0.0.0 --port 5000 --log-config ./config/logging.yml &
 else
-    poetry run uvicorn dripdrop.app:app --host 0.0.0.0 --port 5000 --workers 2 --log-config ./config/logging.yml &
+    poetry run uvicorn dripdrop.app:app --host 0.0.0.0 --port 5000 --workers 4 --log-config ./config/logging.yml &
 fi
 
 SERVER_PID=$!
