8520df6a5a7c91a95afe9dcd416e242b64f0870b
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-04-26 21:37:07 -0400

    fix proxy update to not fail if the service is down

--- dripdrop/apps/admin/utils.py
+++ dripdrop/apps/admin/utils.py
@@ -10,19 +10,19 @@ from .models import Proxy
 PROXY_LIST_URL = "https://proxylist.geonode.com/api/proxy-list"
 
 
-async def get_proxy(session: AsyncSession):
-    async def _get_proxy():
-        query = select(Proxy).order_by(nulls_first(Proxy.last_used.asc()))
-        results = await session.scalars(query)
-        return results.first()
+async def _get_proxy(session: AsyncSession):
+    query = select(Proxy).order_by(nulls_first(Proxy.last_used.asc()))
+    results = await session.scalars(query)
+    return results.first()
 
-    proxy = await _get_proxy()
+
+async def get_proxy(session: AsyncSession):
+    proxy = await _get_proxy(session=session)
     if proxy:
         current_time = dripdrop_utils.get_current_time()
         limit = current_time - timedelta(hours=1)
         if proxy.created_at > limit:
             return proxy
-
     async with http_client.create_client() as client:
         response = await client.get(
             PROXY_LIST_URL,
@@ -36,17 +36,16 @@ async def get_proxy(session: AsyncSession):
                 "speed": "fast",
             },
         )
-    response.raise_for_status()
-    json = response.json()
-    query = delete(Proxy)
-    await session.execute(query)
-    await session.commit()
-    for proxy in json["data"]:
-        query = select(Proxy).where(Proxy.ip_address == proxy["ip"])
-        results = await session.scalars(query)
-        existing_proxy = results.first()
-        if existing_proxy:
-            continue
-        session.add(Proxy(ip_address=proxy["ip"], port=int(proxy["port"])))
+    if response.status_code == 200:
+        json = response.json()
+        query = delete(Proxy)
+        await session.execute(query)
         await session.commit()
-    return await _get_proxy()
+        for proxy in json["data"]:
+            query = select(Proxy).where(Proxy.ip_address == proxy["ip"])
+            results = await session.scalars(query)
+            existing_proxy = results.first()
+            if not existing_proxy:
+                session.add(Proxy(ip_address=proxy["ip"], port=int(proxy["port"])))
+                await session.commit()
+    return await _get_proxy(session=session)
