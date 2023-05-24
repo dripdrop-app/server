85e10613520e35f78ff2dbf8b5e6f87788bc5857
Mohamed Raffik <raffikmohamed7@gmail.com>
2023-05-23 23:53:24 -0400

    add logger to scheduler

--- dripdrop/services/scheduler.py
+++ dripdrop/services/scheduler.py
@@ -3,6 +3,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import timedelta, timezone
 from redis.connection import parse_url
 
+from dripdrop.logger import logger
 from dripdrop.settings import settings
 
 EST = timezone(timedelta(hours=-5))
@@ -11,4 +12,5 @@ scheduler = AsyncIOScheduler(
     jobstores={"default": RedisJobStore(**parse_url(settings.redis_url))},
     timezone=EST,
     job_defaults={"misfire_grace_time": None, "coalesce": True},
+    logger=logger,
 )
